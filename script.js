e7bc2b1841c798dde0c50213b50941ad06383cc5
Annie Hosch <anniehosch@Annies-Air.lan>
2022-07-07 17:13:41 -0400

    updating html, css, and js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-alert("hello!")
+$(document).ready(function(){
+ 
+    $.ajax({
+        type: "GET",
+        url: "http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page=Capitol_Hill_Block_Party&callback=?",
+        contentType: "application/json; charset=utf-8",
+        async: false,
+        dataType: "json",
+        success: function (data, textStatus, jqXHR) {
+ 
+            var markup = data.parse.text["*"];
+            var blurb = $('<div></div>').html(markup);
+            $('#article').html($(blurb).find('p'));
+ 
+        },
+        error: function (errorMessage) {
+        }
+    });
+});
