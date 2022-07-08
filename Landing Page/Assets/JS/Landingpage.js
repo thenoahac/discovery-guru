e2694818a34bb2a0b0aac570a92b21345d206928
Chris <kjman26@gmail.com>
2022-07-08 14:43:00 -0700

    updates

--- Landing Page/Assets/JS/Landingpage.js
+++ Landing Page/Assets/JS/Landingpage.js
@@ -1,32 +1,34 @@
-var btn = document.getElementbyID('#searchBtn')
+let btn = document.getElementById("searchBtn");
 
 
 $('.dropdown-trigger').dropdown()
 $('.dropdown-genre').dropdown()
 $('.dropdown-other').dropdown()
 
-btn.onclick = function(event){
+
+btn.addEventListener("click",function(event){
     // take in values of inputs and store as variable
     var userInput = dateInput + locationInput
     //fetch data 
+    fetch()
     //append to page  
     event.preventDefault(event);
 
     function getInputFromTextBox() {
     var dateInput = document.getElementById("#date-range").value;
-    console.log(input)
+    console.log(dateinput)
     }
 
     function getInputFromTextBox() {
     var locationInput = document.getElementById("#location").value;
     alert(input);
-    }
+    }  })
     // if (homePage.style.display !== "none"){
     //     homePage.style.display="none";
     // } else {
     //     homePage.style.display = "block"
     // };
-    }
+  
 // store data from input fields 
 
 //get values out of inputs
