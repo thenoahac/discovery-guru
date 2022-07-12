c00602d3f9df9c544e714f9ed7146093722711bd
ducky <chrisdmle@gmail.com>
2022-07-12 10:07:50 -0700

    updates to mvp

--- mvp/script.js
+++ mvp/script.js
@@ -1,6 +1,8 @@
 var geocoder;
 var map;
-
+const city = localStorage.getItem('address');
+const startDate = localStorage.getItem('startDate');
+const endDate = localStorage.getItem('endDate');
 // GEOCODER INITIALIZE
 function initialize() {
   geocoder = new google.maps.Geocoder();
@@ -11,8 +13,11 @@ function initialize() {
 function codeAddress() {
   //takes user city input as a value, geocodes - results[0] = latlon
   var address = document.getElementById('address').value;
+  localStorage.setItem('address', address);
   var startDate = document.getElementById('startDate').value;
+  localStorage.setItem('startDate', startDate);
   var endDate = document.getElementById('endDate').value;
+  localStorage.setItem('endDate', endDate);
   geocoder.geocode( { 'address': address}, function(results, status) {
     console.log(results)
     var latlon = results[0].geometry.location.lat() + "," + results[0].geometry.location.lng()
@@ -44,14 +49,18 @@ function codeAddress() {
     }
   });
 }
-
+//$("#eventImg").append("<p>" + json._embedded.events[0].images[0].url + "</p>");
+//i<json.page.size for loop
 
 function showEvents(json) {
-  for(var i=0; i<json.page.size; i++) {
-    $("#events").append("<h4>"+json._embedded.events[i].name+"</h4>");
-    $("#events").append("<p>"+json._embedded.events[i].dates.start.localDate+"</p>");
+  for(var i=0; i<1; i++) {
+    $("#events").append("<h4 class='center-align' style='color:#E9C46A;'>"+json._embedded.events[i].name+"</h4>");
+    $("#events").append("<p class='center-align' style='color:#F4A261;'>"+json._embedded.events[i].dates.start.localDate+"</p>");
+    $("#events").append("<p class='center-align' style='color:#E76F51;'>"+json._embedded.events[i]._embedded.venues[0].name+"</p>");
+    $("#events").append("<img src='" + json._embedded.events[i].images[0].url + "'>");
   }
 }
+
 
 function initMap(results, json) {
   var mapDiv = document.getElementById('map');
@@ -59,7 +68,7 @@ function initMap(results, json) {
     center: {lat: results[0].geometry.location.lat(), lng: results[0].geometry.location.lng()},
     zoom: 10
   });
-  for(var i=0; i<json.page.size; i++) {
+  for(var i=0; i<4; i++) {
     addMarker(map, json._embedded.events[i]);
   }
 }
